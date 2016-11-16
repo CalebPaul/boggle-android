0fb7217b32f59e6000f165ccf01a3d4609c28403
Caleb Paul and Matt Nicholas <matt@>
2016-11-16 11:40:08 -0800

    add random letters generator and some styling

--- app/src/main/java/com/example/epicodus/boggle/MainActivity.java
+++ app/src/main/java/com/example/epicodus/boggle/MainActivity.java
@@ -8,7 +8,12 @@ import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +27,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        String newInput = mInputField.toString();
+        List<Character> startLetters = new ArrayList<>();
+        Random randomGenerator = new Random();
+        Shuffle s = new Shuffle();
+
+
+
+        for(int i = 0; i < 8; i++){
+            if(i==0||i==1){
+                char[] vowels = {'A','E','I','O','U'};
+                int randomNumber = randomGenerator.nextInt(5);
+                startLetters.add(vowels[randomNumber]);
+            }else{
+                int randomNumber = randomGenerator.nextInt(26)+64;
+                char newLetter = (char) randomNumber;
+                startLetters.add(newLetter);
+            }
+
+        }
+        s.shuffle(startLetters.toString());
+        mScrambleTextView.setText(startLetters.toString());
     }
 }
+
+
