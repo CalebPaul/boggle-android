d128d1d219a26a27d98ae1e83b49c4051107c903
Caleb Paul and Matt Nicholas <matt@>
2016-11-16 09:56:22 -0800

    bind initial variables

--- app/src/main/java/com/example/epicodus/boggle/MainActivity.java
+++ app/src/main/java/com/example/epicodus/boggle/MainActivity.java
@@ -2,8 +2,21 @@ package com.example.epicodus.boggle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import butterknife.Bind;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.scrambleTextView) TextView mScrambleTextView;
+    @Bind(R.id.wordListView) TextView mWordListView;
+    @Bind(R.id.inputField) EditText mInputField;
+    @Bind(R.id.submitButton) Button mSubmitButton;
+    @Bind(R.id.refreshButton) Button mRefreshButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
